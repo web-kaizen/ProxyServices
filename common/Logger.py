05e24ebca38131d1b2da86205b056b2a1341eb3c
WooDMaNbtw <nikita.profatilov@gmail.com>
2023-12-06 01:12:41 +0400

    Обновлены методы класса Logger и поля таблицы Log

--- common/Logger.py
+++ common/Logger.py
@@ -1,57 +1,87 @@
 import os
 from datetime import datetime
-from .LogModel import Log
+from django.conf import settings
+from .models import Log
 
 class Logger:
-    NEED_LOGGER: bool = bool(os.getenv("NEED_LOGGER"))
+    NEED_LOGGER: bool = settings.NEED_LOGGER
 
-    def __init__(self):
-        self.log_entry = Log()
+    def __init__(self,
+                 proxy_method=None,
+                 core_method=None,
+                 proxy_url=None,
+                 core_url=None,
+                 proxy_request_headers=None,
+                 core_request_headers=None,
+                 proxy_request_body=None,
+                 core_request_body=None,
+                 proxy_response_headers=None,
+                 core_response_headers=None,
+                 proxy_response_body=None,
+                 core_response_body=None,
+                 proxy_response_status_code=None,
+                 core_response_status_code=None,
+                 ):
+        self.__log_entry = Log()
+        self._set_proxy_method(method=proxy_method)
+        self._set_core_method(method=core_method)
+        self._set_proxy_url(url=proxy_url)
+        self._set_core_url(url=core_url)
+        self._set_proxy_request_headers(headers=proxy_request_headers)
+        self._set_core_request_headers(headers=core_request_headers)
+        self._set_proxy_request_body(body=proxy_request_body)
+        self._set_core_request_body(body=core_request_body)
+        self._set_proxy_response_headers(headers=proxy_response_headers)
+        self._set_core_response_headers(headers=core_response_headers)
+        self._set_proxy_response_body(body=proxy_response_body)
+        self._set_core_response_body(body=core_response_body)
+        self._set_proxy_response_status_code(status_code=proxy_response_status_code)
+        self._set_core_response_status_code(status_code=core_response_status_code)
 
 
-    def set_proxy_method(self, method):
-        self.log_entry.proxy_method = method
+    def _set_proxy_method(self, method):
+        self.__log_entry.proxy_method = method
 
-    def set_core_method(self, method):
-        self.log_entry.core_method = method
+    def _set_core_method(self, method):
+        self.__log_entry.core_method = method
 
-    def set_proxy_url(self, url):
-        self.log_entry.proxy_url = url
+    def _set_proxy_url(self, url):
+        self.__log_entry.proxy_url = url
 
-    def set_core_url(self, url):
-        self.log_entry.core_url = url
+    def _set_core_url(self, url):
+        self.__log_entry.core_url = url
 
-    def set_proxy_request_headers(self, headers):
-        self.log_entry.proxy_request_headers = headers
+    def _set_proxy_request_headers(self, headers):
+        self.__log_entry.proxy_request_headers = headers
 
-    def set_core_request_headers(self, headers):
-        self.log_entry.core_request_headers = headers
+    def _set_core_request_headers(self, headers):
+        self.__log_entry.core_request_headers = headers
 
-    def set_proxy_request_body(self, body):
-        self.log_entry.proxy_request_body = body
+    def _set_proxy_request_body(self, body):
+        self.__log_entry.proxy_request_body = body
 
-    def set_core_request_body(self, body):
-        self.log_entry.core_request_body = body
+    def _set_core_request_body(self, body):
+        self.__log_entry.core_request_body = body
 
-    def set_proxy_response_headers(self, headers):
-        self.log_entry.proxy_response_headers = headers
+    def _set_proxy_response_headers(self, headers):
+        self.__log_entry.proxy_response_headers = headers
 
-    def set_core_response_headers(self, headers):
-        self.log_entry.core_response_headers = headers
+    def _set_core_response_headers(self, headers):
+        self.__log_entry.core_response_headers = headers
 
-    def set_proxy_response_body(self, body):
-        self.log_entry.proxy_response_body = body
+    def _set_proxy_response_body(self, body):
+        self.__log_entry.proxy_response_body = body
 
-    def set_core_response_body(self, body):
-        self.log_entry.core_response_body = body
+    def _set_core_response_body(self, body):
+        self.__log_entry.core_response_body = body
 
-    def set_proxy_response_status_code(self, status_code):
-        self.log_entry.proxy_response_status_code = status_code
+    def _set_proxy_response_status_code(self, status_code):
+        self.__log_entry.proxy_response_status_code = status_code
 
-    def set_core_response_status_code(self, status_code):
-        self.log_entry.core_response_status_code = status_code
+    def _set_core_response_status_code(self, status_code):
+        self.__log_entry.core_response_status_code = status_code
 
     def write(self):
         if self.NEED_LOGGER:
-            self.log_entry.created_at = datetime.now().isoformat()
-            self.log_entry.save()
+            self.__log_entry.created_at = datetime.now().isoformat()
+            self.__log_entry.save()
--- common/models.py
+++ common/models.py
@@ -1,4 +1,3 @@
-
 from django.db import models
 
 
@@ -6,24 +5,22 @@ class Log(models.Model):
 
     created_at = models.DateTimeField(auto_now_add=True)
     """PROXY FIELDS"""
-    proxy_method = models.CharField(max_length=10)
-    proxy_url = models.URLField(max_length=200)
+    proxy_method = models.CharField(max_length=10, null=True, blank=True)
+    proxy_url = models.URLField(max_length=255, null=True, blank=True)
     proxy_request_headers = models.JSONField(null=True, blank=True)
     proxy_request_body = models.JSONField(null=True, blank=True)
     proxy_response_headers = models.JSONField(null=True, blank=True)
     proxy_response_body = models.JSONField(null=True, blank=True)
-    proxy_response_status_code = models.PositiveIntegerField()
+    proxy_response_status_code = models.PositiveIntegerField(null=True, blank=True)
 
     '''CORE FIELDS'''
-    core_method = models.CharField(max_length=10)
-    core_url = models.URLField(max_length=200)
+    core_method = models.CharField(max_length=10, null=True, blank=True)
+    core_url = models.URLField(max_length=255, null=True, blank=True)
     core_request_headers = models.JSONField(null=True, blank=True)
     core_request_body = models.JSONField(null=True, blank=True)
     core_response_headers = models.JSONField(null=True, blank=True)
     core_response_body = models.JSONField(null=True, blank=True)
-    core_response_status_code = models.PositiveIntegerField()
-
-
+    core_response_status_code = models.PositiveIntegerField(null=True, blank=True)
 
     def __str__(self):
         return f"Created at - {self.created_at} | Proxy method - {self.proxy_method} | Core method - {self.core_method}"

9d43c6cddd6b74bfd648bf286b098965b128c8e9
Stogov Egor <severeff@gmail.com>
2023-12-08 07:53:22 +0100

    исправлено имя вьюшки "logout"

--- users/urls.py
+++ users/urls.py
@@ -11,7 +11,7 @@ from users.views import (
 urlpatterns = [
     path('', UserRegistrationView.as_view(), name='register'),
     path('login', LoginView.as_view(), name='login'),
-    path('logout', LogoutView.as_view(), name='login'),
+    path('logout', LogoutView.as_view(), name='logout'),
     path("email-verification/check", EmailVerificationCheckView.as_view(), name="email_check"),
     path("email-verification/resend", EmailVerificationResendView.as_view(), name="email_resend"),
     path("email-verification/verify", EmailVerificationVerifyView.as_view(), name="email_login")

41172d57d3fa946a8fb07897fbf832ba88c84466
maksim.lazko <m.lazko@naveksoft.com>
2023-12-09 00:02:35 +0300

    PRO1-178 moved logger to extended app #time 5m

--- logger/models.py
+++ logger/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 
-class Log(models.Model):
+class LogModel(models.Model):
 
     created_at = models.DateTimeField(auto_now_add=True)
     """PROXY FIELDS"""
@@ -13,7 +13,7 @@ class Log(models.Model):
     proxy_response_body = models.JSONField(null=True, blank=True)
     proxy_response_status_code = models.PositiveIntegerField(null=True, blank=True)
 
-    '''CORE FIELDS'''
+    """CORE FIELDS"""
     core_method = models.CharField(max_length=10, null=True, blank=True)
     core_url = models.URLField(max_length=255, null=True, blank=True)
     core_request_headers = models.JSONField(null=True, blank=True)
@@ -23,9 +23,9 @@ class Log(models.Model):
     core_response_status_code = models.PositiveIntegerField(null=True, blank=True)
 
     def __str__(self):
-        return f"Created at - {self.created_at} | Proxy method - {self.proxy_method} | Core method - {self.core_method}"
+        return f'Created at - {self.created_at} | Proxy method - {self.proxy_method} | Core method - {self.core_method}'
 
     class Meta:
-        ordering = ["id"]
-        verbose_name = "Log"
-        verbose_name_plural = "Logs"
+        ordering = ['id',]
+        verbose_name = 'Log'
+        verbose_name_plural = 'Logs'

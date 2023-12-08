16a406cd9a787f3d118a22219d4e3f83c683f3aa
maksim.lazko <m.lazko@naveksoft.com>
2023-12-09 00:54:08 +0300

    fixed settings file

--- dialogues/views.py
+++ dialogues/views.py
@@ -1,9 +1,8 @@
 from typing import Any
 from urllib.request import Request
 
-from rest_framework.views import APIView
-
 from rest_framework.response import Response
+from rest_framework.views import APIView
 
 
 class DialoguesView(APIView):

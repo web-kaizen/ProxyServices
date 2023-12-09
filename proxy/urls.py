94b054a06dd4567550c444a4f9097a01d6afcbf2
maksim.lazko <m.lazko@naveksoft.com>
2023-12-09 16:26:24 +0300

    PRO1-178 updated settings file

--- proxy/urls.py
+++ proxy/urls.py
@@ -30,5 +30,6 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('api/v0/users/', include('users.urls')),
     path('api/v0/bots/', include('bots.urls')),
+    path('api/v0/dialogues/', include('dialogues.urls')),
     path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]

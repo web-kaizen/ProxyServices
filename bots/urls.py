9067a25fbaf0f085dd18a3fca1fb17df39ef8ef2
maksim.lazko <m.lazko@naveksoft.com>
2023-12-09 16:25:52 +0300

    PRO1-178 refactored bots app

--- bots/urls.py
+++ bots/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from bots.views import BotDetailsView, BotListView
 
+
 urlpatterns = [
     path('', BotListView.as_view(), name='bot-list'),
     path('<int:bot_id>/', BotDetailsView.as_view(), name='bot-details'),
--- bots/views.py
+++ bots/views.py
@@ -1,43 +1,31 @@
 from typing import Any
+from urllib.request import Request
 
-from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from bots.services import BotDetails, BotList
+from common.Route import Route
 
 
-class __BaseBotsOperationView(APIView):
-    """Base class for bots list and details"""
-
-    route_class: Any = None
-    endpoint: str = None
+class BotListView(APIView):
+    """Returns the list of existed bots"""
 
     def get(self, request: Request, *args: Any, **kwargs: dict) -> Response:
-        bot_list_route = self.route_class()
-        bot_list_route.set_headers(request.headers)
-        response = bot_list_route.send(
-            endpoint=f"{self.endpoint}{kwargs.get('bot_id') if kwargs.get('bot_id') is not None else ''}",
-            method=request.method,
-            headers=request.headers
-        )
+        response = Route(request=request).send(endpoint='bots')
         return Response(
             data=response.json(),
             status=response.status_code,
+            headers=response.headers
         )
 
 
-class BotListView(__BaseBotsOperationView):
-    """Returns the list of existed bots"""
+class BotDetailsView(APIView):
+    """Returns details for a specific bot"""
 
-    route_class = BotList.BotList
-    endpoint = 'bots'
-
-
-class BotDetailsView(__BaseBotsOperationView):
-    """Returns the list of existed bots"""
-
-    route_class = BotList.BotList
-    endpoint = "bots/"
-
-
+    def get(self, request: Request, *args: Any, **kwargs: dict) -> Response:
+        response = Route(request=request).send(endpoint=f'bots/{kwargs.get("bot_id")}')
+        return Response(
+            data=response.json(),
+            status=response.status_code,
+            headers=response.headers
+        )

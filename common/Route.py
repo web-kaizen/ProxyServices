8ab144e32c76da02fcf0b9f83375cfc61493034a
maksim.lazko <m.lazko@naveksoft.com>
2023-12-09 16:27:00 +0300

    PRO1-178 refactored Route class, rm CustomRoute

--- common/Route.py
+++ common/Route.py
@@ -2,54 +2,41 @@ import requests
 from django.conf import settings
 from requests import Response
 
+from rest_framework.request import Request
+
 
 class Route:
     """Base Route for communication with third-party services"""
 
-    __APP_ID: str = settings.APP_ID
-    _parameters: dict = {}
-    _response: Response = None
-    _headers: dict = {}
+    def __init__(self, request: Request) -> None:
+        self.request = request
+        self.__APP_ID: str = settings.APP_ID
 
-    def set_headers(self, headers: dict) -> None:
-        self._headers = headers
+    @property
+    def request_method(self) -> str:
+        return self.request.method
 
-    def get_headers(self) -> dict:
-        return self._headers
+    @property
+    def request_data(self) -> property:
+        return self.request.data
 
-    def set_parameters(self, params: dict) -> None:
-        self._parameters = params
+    @property
+    def request_headers(self) -> dict:
+        return self.request.headers
 
-    def get_parameters(self) -> dict:
-        return self._parameters
+    @property
+    def request_query_params(self) -> property:
+        return self.request.query_params
 
-    def set_response(self, response: Response, status: int = None) -> None:
-        if status is not None:
-            if 200 <= status < 300:
-                response = self.on_success(response)
-            if 400 <= status <= 500:
-                response = self.on_error(response)
-        self._response = response
-
-    def get_response(self) -> Response:
-        return self._response
-
-    def send(self, endpoint: str, method: str, **kwargs: dict) -> Response:
+    def send(self, endpoint: str, **kwargs: dict) -> Response:
         response = requests.request(
-            method=method,
+            method=self.request_method,
             url=f'{settings.THIRD_PARTY_APP_URL}/{self.__APP_ID}/{endpoint}/',
-            json=self.get_parameters(),
-            # headers=self.get_headers(),
+            data=self.request_data,
+            # headers=self.request_headers,
+            params=self.request_query_params,
             **kwargs
         )
-        del response.headers["Connection"]
-        del response.headers["Keep-Alive"]
-        return response
-
-    @staticmethod
-    def on_success(response: Response) -> Response:
-        return response
-
-    @staticmethod
-    def on_error(response: Response) -> Response:
+        del response.headers['Connection']
+        del response.headers['Keep-Alive']
         return response

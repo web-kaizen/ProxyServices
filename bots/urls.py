b38efa157e9337ce75deda66682579d5e2436221
maksim.lazko <m.lazko@naveksoft.com>
2023-12-17 16:31:28 +0300

    isort fixes

--- bots/urls.py
+++ bots/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 
 from bots.views import BotView
 
-
 urlpatterns = [
     path('', BotView.as_view(), name='bot-list'),
     path('<int:bot_id>/', BotView.as_view(), name='bot-detail'),
